be876073dabf74dae0e5315d48fbfd4d2bf94f06
michaelparkernorton <michaelparkernorton.va@gmail.com>
2025-07-22 17:19:04 -0400

    Directory Update 2

--- tests/test_smokeTest.py
+++ tests/test_smokeTest.py
@@ -10,6 +10,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class TestSmokeTest():
